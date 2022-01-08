896d87fd908ebb203f5aa174ecd253267e916648
luketwilson76 <78549037+luketwilson76@users.noreply.github.com>
2022-01-07 19:38:52 -0500

    Scripting
    Mirror Animation
    Transportation Script
    Mirror Object Prefab

--- Ripple/Assets/Scripts/PlayerMovement.cs
+++ Ripple/Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-
     public float speed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
-
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
